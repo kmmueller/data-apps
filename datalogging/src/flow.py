603e7ee09d7a0c7294aa04c3c65834ac36a4c485
kmmueller <muellerkmrks@gmail.com>
2015-02-07 16:50:41 -0800

    Initial commit for flow to tie together other functions for making the datatables in the database and generating the excel file.

--- datalogging/src/flow.py
+++ datalogging/src/flow.py
@@ -8,9 +8,17 @@ the excel workbook.  I will try first just looping and building one by one
 and see if this strategy works.
 first, set up the variables for one of the stocks
 '''
+import logTableGen
+loopDone="no"
+while (loopDone=="no"):
+    ticker = input('Enter stock ticker: ')
+    expDate = input('Enter the expiration date: ')
+    lowStrike = float(input('Enter the low strike: '))
+    step = float(input('Enter the step size between strikes: '))
+    numStrikes = float(input('Enter the number of strikes: '))
+    
+    tableName=logTableGen.logTableGen(ticker, expDate, lowStrike, step, numStrikes)
+    print('The table created is ', tableName)
+    loopDone= input('Are you finished? Please enter yes or no. ')
 
-ticker = input('Enter stock ticker: ')
-expDate = input('Enter the expiration date: ')
-lowStrike = float(input('Enter the low strike: '))
-step = float(input('Enter the step size between strikes: '))
-numStrikes = float(input('Enter the number of strikes: '))
+print('Congratulations - we are done. ')
