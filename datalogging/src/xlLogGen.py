4e4423e3a7626cb1fe92fac400a2133720bd1fd7
kmueller <muellerkmrks@gmail.com>
2015-03-08 03:39:12 -0700

    Initial commit for xlLogGen.  Separates the excel file generation into a separate module to be called from the flow module.

---
+++ datalogging/src/xlLogGen.py
@@ -0,0 +1,66 @@
+'''
+Created on Mar 8, 2015
+Extracting the function from FloatLogGen2 to make it standalone and callable from the flow module.
+@author: dev-machine
+'''
+import re
+import numpy as np
+from openpyxl import Workbook
+
+def xlLogGen(ticker, expDate, lowStrike, step, numStrikes,wb):
+    
+    # Build an array of the strike prices based on low, step and number of strikes
+    # Get the max price based on steps, low, number of strikes
+    maxStrike=(lowStrike+(numStrikes)*step)
+    print('Max Strike=',maxStrike)
+    strikes=np.arange(lowStrike,maxStrike,step) #numpy arange - allows for float values for min, max, and step
+
+    callName="."+ticker+expDate+'C'
+    putName="."+ticker+expDate+'P'
+    #wb=Workbook()
+    ws=wb.active
+    dataItemNames=["=RTD(\"tos.rtd\", ,\"LAST\", \""+ticker+"\")"]
+    # playing with writing to openpyxl. Here I have setup the workbook object in memory, set the first sheet to active, and
+    # now am writing the first row as the ticker LAST value.  I think I can use this format to write out multiple stocks to 
+    # the same page
+    ws.append(dataItemNames)
+    xx=[]
+    for x in (strikes):
+        m=re.compile('\d+')
+        n=m.findall(str(x))
+        if (n[1]=='0'):
+            x=int(x)
+            print('This is the new x:', x)
+        else:
+            x=n[0]+"."+n[1]
+            print('This is new x with a decimal')
+        #print(m.group(0))
+        callAsk="=RTD(\"tos.rtd\", , \"ASK\", \""+callName+str(x)+"\")"
+        callAskSize="=RTD(\"tos.rtd\", , \"ASK_SIZE\", \""+callName+str(x)+"\")"
+        callBid="=RTD(\"tos.rtd\", , \"BID\", \""+callName+str(x)+"\")"
+        callBidSize="=RTD(\"tos.rtd\", , \"BID_SIZE\", \""+callName+str(x)+"\")"
+        callVolume="=RTD(\"tos.rtd\", , \"VOLUME\", \""+callName+str(x)+"\")"
+        callOpenInt="=RTD(\"tos.rtd\", , \"OPEN_INT\", \""+callName+str(x)+"\")"
+        callDelta="=RTD(\"tos.rtd\", , \"DELTA\", \""+callName+str(x)+"\")"
+        callTheta="=RTD(\"tos.rtd\", , \"THETA\", \""+callName+str(x)+"\")"
+        callGamma="=RTD(\"tos.rtd\", , \"GAMMA\", \""+callName+str(x)+"\")"
+        callVega="=RTD(\"tos.rtd\", , \"VEGA\", \""+callName+str(x)+"\")"
+        callRho="=RTD(\"tos.rtd\", , \"RHO\", \""+callName+str(x)+"\")"
+        putAsk="=RTD(\"tos.rtd\", , \"ASK\", \""+putName+str(x)+"\")"
+        putAskSize="=RTD(\"tos.rtd\", , \"ASK_SIZE\", \""+callName+str(x)+"\")"
+        putBid="=RTD(\"tos.rtd\", , \"BID\", \""+callName+str(x)+"\")"
+        putBidSize="=RTD(\"tos.rtd\", , \"BID_SIZE\", \""+callName+str(x)+"\")"
+        putVolume="=RTD(\"tos.rtd\", , \"VOLUME\", \""+callName+str(x)+"\")"
+        putOpenInt="=RTD(\"tos.rtd\", , \"OPEN_INT\", \""+callName+str(x)+"\")"
+        putBid="=RTD(\"tos.rtd\", , \"BID\", \""+putName+str(x)+"\")"
+        putDelta="=RTD(\"tos.rtd\", , \"DELTA\", \""+putName+str(x)+"\")"
+        putTheta="=RTD(\"tos.rtd\", , \"THETA\", \""+putName+str(x)+"\")"
+        putGamma="=RTD(\"tos.rtd\", , \"GAMMA\", \""+putName+str(x)+"\")"
+        putVega="=RTD(\"tos.rtd\", , \"VEGA\", \""+putName+str(x)+"\")"
+        putRho="=RTD(\"tos.rtd\", , \"RHO\", \""+putName+str(x)+"\")"
+        xx.append([callAsk,callAskSize,callBid,callBidSize,callVolume,callOpenInt,callDelta,callTheta,callGamma,callVega,callRho,putAsk,putAskSize,putBid,putBidSize,putVolume,putOpenInt,putDelta,putTheta,putGamma,putVega,putRho])
+        ws.append([callAsk,callAskSize,callBid,callBidSize,callVolume,callOpenInt,callDelta,callTheta,callGamma,callVega,callRho,putAsk,putAskSize,putBid,putBidSize,putVolume,putOpenInt,putDelta,putTheta,putGamma,putVega,putRho])
+        dataItemColumns=(callAsk,callAskSize,callBid,callBidSize,callVolume,callOpenInt,callDelta,callTheta,callGamma,callVega,callRho,putAsk,putAskSize,putBid,putBidSize,putVolume,putOpenInt,putDelta,putTheta,putGamma,putVega,putRho)
+        for y in (dataItemColumns):
+            dataItemNames.append(y)
+    return(wb)
