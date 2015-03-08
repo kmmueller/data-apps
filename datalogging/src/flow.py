e0ade112d1054e151e0c1bb9a9c38218d59c4888
kmueller <muellerkmrks@gmail.com>
2015-03-08 03:57:06 -0700

    Added xls generation to the loop.  Something's not working right yet though...

--- datalogging/src/flow.py
+++ datalogging/src/flow.py
@@ -2,6 +2,7 @@
 Making a main program to call the routines and build the excel workbook.
 2/7/2015
 '''
+from openpyxl.workbook import Workbook
 '''
 Idea is to have a way to set up an arbitrary number of stocks adn generate
 the excel workbook.  I will try first just looping and building one by one
@@ -9,7 +10,9 @@ and see if this strategy works.
 first, set up the variables for one of the stocks
 '''
 import logTableGen
+import xlLogGen
 loopDone="no"
+ww=Workbook()
 while (loopDone=="no"):
     ticker = input('Enter stock ticker: ')
     expDate = input('Enter the expiration date: ')
@@ -18,7 +21,9 @@ while (loopDone=="no"):
     numStrikes = float(input('Enter the number of strikes: '))
     
     tableName=logTableGen.logTableGen(ticker, expDate, lowStrike, step, numStrikes)
+    ww=xlLogGen.xlLogGen(ticker, expDate, lowStrike, step, numStrikes,ww)
     print('The table created is ', tableName)
     loopDone= input('Are you finished? Please enter yes or no. ')
-
+xlFileName= input('Enter name for your new excel logging file: ')
+ww.save(xlFileName)
 print('Congratulations - we are done. ')
