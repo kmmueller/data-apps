b7a9747b5b3bdfa69ea34a27cd5c6f71e3d72289
jrbrowning <jrbpdx@gmail.com>
2015-03-24 19:32:30 -0700

    Pushing working version into local repo

--- datalogging/src/DataPortalInput.py
+++ datalogging/src/DataPortalInput.py
@@ -12,13 +12,6 @@ import xlLogGen
 from InputConfigs import *
 
 ww=Workbook()
-#ExpList = ['150327','150331','150402','150410','150417','150424','150501','150515','150619','150630','150717','150918','150930','151219','151231','160115']
-#ExpList = ['150327','150331','150402','150410','150417','150424','150501','150515','150619','150630','150717','150918','150930','151219','151231','160115']
-#ticker = 'SPY'
-#lowStrike = float(190)
-#step = float(1)
-#numStrikes = float(40)  # 22 columns generated.  40 numStrikes is OK
-
 for expDate in ExpList:
     tableName=logTableGen.logTableGen(ticker, expDate, lowStrike, step, numStrikes)
     ww=xlLogGen.xlLogGen(ticker, expDate, lowStrike, step, numStrikes,tableName,ww)
