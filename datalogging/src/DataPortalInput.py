1b81df592b2b943eb153cdc94f21ec7b16c10844
jrbrowning <jrbpdx@gmail.com>
2015-03-29 15:45:47 -0700

    Updating initial copy of file for reference later

--- datalogging/src/DataPortalInput.py
+++ datalogging/src/DataPortalInput.py
@@ -9,14 +9,24 @@ Take existing FLOW.py module and wrap with loop so as to build a single stock ex
 
 import logTableGen
 import xlLogGen
-from InputConfigs import *
+
+#ConfigFile = input('Enter name for the config file ')
+
+def getVarFromFile(filename):
+    import imp
+    f = open(filename,'r')
+    global data
+    data = imp.load_source('data', '', f)
+    f.close()
+
+getVarFromFile('c:\InputValues.txt')
 
 ww=Workbook()
-for expDate in ExpList:
-    tableName=logTableGen.logTableGen(ticker, expDate, lowStrike, step, numStrikes)
-    ww=xlLogGen.xlLogGen(ticker, expDate, lowStrike, step, numStrikes,tableName,ww)
+for expDate in data.ExpList:
+    tableName=logTableGen.logTableGen(data.ticker, expDate, data.lowStrike, data.step, data.numStrikes)
+    ww=xlLogGen.xlLogGen(data.ticker, expDate, data.lowStrike, data.step, data.numStrikes,tableName,ww)
     
-xlFileName= '%s%d%d%d.xlsx' % (ticker,lowStrike,numStrikes,step)
+xlFileName= '%s%d%d%d.xlsx' % (data.ticker,data.lowStrike,data.numStrikes,data.step)
 print(xlFileName)
 ww.save(xlFileName)
 print('Congratulations - we are done.  Time for beer!')
