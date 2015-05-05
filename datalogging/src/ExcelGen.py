0cd93d8e8d42d11ef4690af85420fd4a8ef9b7b6
kmmueller <muellerkmrks@gmail.com>
2015-05-04 20:38:00 -0700

    Changed file to generate 40 strikes of SPY.

--- datalogging/src/ExcelGen.py
+++ datalogging/src/ExcelGen.py
@@ -12,13 +12,13 @@ import xlLogGen
 #from InputConfigs import *
 
 ww=Workbook()
-ExpList = ['150410','150417','150424','150501','150508','150515','150522','150619','150630','150717','150918','150930','151219','151231','160115']
+ExpList = ['150417','150424','150501','150508','150515','150522','150529','150619','150630','150717','150918','150930','151219','151231','160115','160318','160617','160916','161216','170120','170317','171215']
 #ExpList = ['150327','150331','150402','150410','150417','150424','150501','150515','150619','150630','150717','150918','150930','151219','151231','160115']
-ticker = 'GPRO'
-lowStrike = float(31)
-step = float(1)
-numStrikes = float(20)  # 22 columns generated.  40 numStrikes is OK
-suffix = '_ww15'
+ticker = 'SPY'
+lowStrike = float(200)
+step = float(0.5)
+numStrikes = float(40)  # 22 columns generated.  40 numStrikes is OK
+suffix = '_ww16'
 
 for expDate in ExpList:
     tableName=logTableGen.logTableGen(ticker, expDate, lowStrike, step, numStrikes)
